3dc2f3907866f2454a96749c88fc7fca0787b0e5
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-07-29 16:29:59 +0300

    Again

--- source/RPGState.hx
+++ source/RPGState.hx
@@ -280,10 +280,10 @@ class RPGState extends MusicBeatState
 		#if android
 		if (player != null && joystick != null)
 		{
-			player.left = (joystick.justPressed && (joystick.getAngle() > 120 || joystick.getAngle() < -120));
-			player.right = (joystick.justPressed && joystick.getAngle() > -60 && joystick.getAngle() < -60);
-			player.up = (joystick.justPressed && joystick.getAngle() > 30 && joystick.getAngle() < 150);
-			player.down = (joystick.justPressed && joystick.getAngle() > -150 && joystick.getAngle() < -30);
+			player.left = (joystick._dir == 'Left');
+			player.right = (joystick._dir == 'Right');
+			player.up = (joystick._dir == 'Up');
+			player.down = (joystick._dir == 'Down');
 		}
 		#end
 	}
